5d7909ac945e78ecafb83ab4ee32dbbdeca532bb
Pradip Gupta <pradip@revelationtech.com>
2025-08-28 18:28:11 +0000

    Date Dimension Model

---
+++ models/date_dimension.sql
@@ -0,0 +1,35 @@
+WITH cte AS (
+    SELECT 
+        TO_TIMESTAMP(started_at) AS STARTED_AT,
+        DATE(TO_TIMESTAMP(started_at)) AS DATE_STARTED_AT,
+        HOUR(TO_TIMESTAMP(started_at)) AS HOUR_STARTED_AT,
+        MINUTE(TO_TIMESTAMP(started_at))  AS MINUTE_STARTED_AT,
+        DAYNAME(TO_TIMESTAMP(started_at)) AS DAY_OF_WEEK,
+
+
+        CASE
+        WHEN DAYNAME(TO_TIMESTAMP(started_at)) in ('Sat','Sun')
+        THEN 'Weekend'
+        ELSE 'BusinessDay'
+        END AS DAY_TYPE,
+
+        CASE
+        WHEN MONTH(TO_TIMESTAMP(started_at)) IN (12,1,2)
+        THEN 'Winter'
+        WHEN MONTH(TO_TIMESTAMP(started_at)) IN (3,4,5)
+        THEN 'Spring'
+        WHEN MONTH(TO_TIMESTAMP(started_at)) IN (6,7,8)
+        THEN 'Summer'
+        WHEN MONTH(TO_TIMESTAMP(started_at)) IN (12,1,2)
+        THEN 'Autumn'
+        END AS SESSION
+
+
+
+
+    FROM {{ source('demo', 'bike') }}
+    WHERE started_at != 'started_at'
+)
+
+SELECT *
+FROM cte
