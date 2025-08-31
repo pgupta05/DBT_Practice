05001ff81a84f21e84d9812c26cb34403eb057d2
Pradip Gupta <pradip@revelationtech.com>
2025-08-31 00:03:50 +0000

    Last commit

--- models/weather_bike_correlation.sql
+++ models/weather_bike_correlation.sql
@@ -7,10 +7,11 @@ from {{ ref('trip_fact') }} t
 left join {{ ref('daily_weather') }} w
 on t.TRIP_DATE = w.daily_weather
 
-Order by t.TRIP_DATE
+
 )
 
 
 select 
 * 
 FROM CTE
+-- ORDER BY TRIP_DATE
